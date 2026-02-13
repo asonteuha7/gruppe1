f45901458d98f25881684f5bdbccefda7be2345f
gaffelsteinkh <GaffelsteinKH@gmail.com>
2026-02-13 11:15:44 +0100

    Added Echo func in goggen.cs

--- Goggen.cs
+++ Goggen.cs
@@ -4,6 +4,15 @@ public class Goggen
     public static bool PWD(string[] args)
     {
         Console.WriteLine(Directory.GetCurrentDirectory());
-        return false; //placeholder
+        return false; 
     }
+
+    public static bool Echo(string[] args)
+    {
+        string? input = Console.ReadLine();
+        string[] text = input!.Split(' ');
+        string message = string.Join(" ", text[1..]);
+        Console.WriteLine(message);
+        return false;
+    }  
 }
