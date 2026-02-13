a01f1b1d823eefcacfe6e0cb85a3930e1750cfbd
asonteuha7 <senad.26sfj@gmail.com>
2026-02-13 10:20:42 +0100

    file

---
+++ David.cs
@@ -0,0 +1,9 @@
+class David{
+    public static bool help(string[] args){
+        Console.WriteLine("Available commands");
+        foreach(var arg in args){
+            Console.WriteLine(arg);
+        }
+        return true;
+    }
+}
